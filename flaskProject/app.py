b7680fa16013757dd778465492d0c98c962e6607
Сергей Антоньянец <sergey.antonyanets@gmail.com>
2022-09-28 17:01:55 +0300

    flask project html, css, python

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    print(url_for("index"))
+    return render_template("index.html")
+
+
+@app.route('/about/<int:username>')
+def about(username: str):
+    return render_template("about.html", username = username)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
