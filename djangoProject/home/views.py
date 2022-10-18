37511eb757cccff304fa05f0121be5e201b9d0a8
Сергей Антоньянец <sergey.antonyanets@gmail.com>
2022-10-18 13:27:07 +0300

    django

---
+++ djangoProject/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("this is home page")
+
+def mybesthome(request):
+    return render(request, 'index.html')
